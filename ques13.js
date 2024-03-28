d698289e943c3ddef47445feeca353f8e7c27d08
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:22:24 +0500

    Add files via upload

---
+++ ques13.js
@@ -0,0 +1,6 @@
+"use strict";
+//13.Your Own Array.Think of your favourite mode of transpotation ,suc as a motorcycle or a car and make a list that stor several examples.Use a list to print a series of statments about these items,Such as I would liake to own a Honda motor cycle.
+let transport = ["Honda motarcycle", "Corolla", "Hilux"];
+transport.map((item) => {
+    console.log(`I would like to own a ${item}.`);
+});
